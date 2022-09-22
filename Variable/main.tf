e4c377263be60fb9a4f63cf9e93a813b35158b01
Rufat Zeynalov <57935226+RufatZeynalov@users.noreply.github.com>
2022-09-22 13:48:31 +0200

    Update main.tf

--- Variable/main.tf
+++ Variable/main.tf
@@ -1,6 +1,6 @@
 provider "aws" {
-  access_key = "AKIARJ5LAJYCOIXAVGFC"  # user aceess key
-  secret_key = "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"  # user secret key
+  access_key = "--------------------------------"  # user aceess key
+  secret_key = "--------------------------------"  # user secret key
   region = var.region     # relocation data to variable by var.data function
 }
 
